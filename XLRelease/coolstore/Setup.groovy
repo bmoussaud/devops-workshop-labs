ffea57fe08ed06c8983991c7f1df425e3fe887aa
Benoit Moussaud <bmoussaud@xebialabs.com>
2018-10-02 11:42:13 +0200

    a

---
+++ XLRelease/coolstore/Setup.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://localhost:5516/#/templates/Folderd9d4adaacdbe454cb7d27adc14a1252d-Release30894a3c2e94447ab0059f0d35bbe0cd/releasefile
+// XL Release version:   8.2.0
+// Date created:         Tue Oct 02 11:42:10 CEST 2018
+
+xlr {
+  template('Setup') {
+    folder('coolstore')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-10-02T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
